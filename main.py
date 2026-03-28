fdd7a4427978558eb0e77b14026ba899216e4455
theeditor <theeditor264@gmail.com>
2026-03-28 10:07:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = "@corn_adult_content"
 
 messages = [
-    “New video dropping soon… stay ready 👀🔥”,
-    “Some secrets are too good to stay hidden 🔥 @sexxy_korean_girls”,
-    “Next post is gonna be crazy… coming shortly 🔥”,
-    “If you like this… you’ll LOVE what’s there 💋 @sexxy_korean_girls",
-    “Uploading something special… don’t go anywhere 😏”,
-    “You deserve better content than this 😉 👉 @sexxy_korean_girls”,
-    “Fresh content is on the way… stay tuned 👀”,
-    “New drop in a few minutes… get ready 😈”,
+    "New video dropping soon... stay ready 👀🔥",
+    "Some secrets are too good to stay hidden 🔥 @sexxy_korean_girls",
+    "Next post is gonna be crazy... coming shortly 🔥",
+    "If you like this... you'll LOVE what's there 💋 @sexxy_korean_girls",
+    "Uploading something special... don't go anywhere 😏",
+    "You deserve better content than this 😉 👉 @sexxy_korean_girls",
+    "Fresh content is on the way... stay tuned 👀",
+    "New drop in a few minutes... get ready 😈"
 ]
 
 def send_message():
@@ -27,4 +27,5 @@ def send_message():
 while True:
     send_message()
     print("Message sent!")
-    time.sleep(600)
+    delays = [1200, 1800, 30000, 3600, 7200]
+    time.sleep(random.choice(delays))
